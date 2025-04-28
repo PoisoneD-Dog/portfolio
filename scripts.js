c208177b052b1773b3d8e99801f95285e1be7b18
Yaku <yakuzalchicha@gmail.com>
2025-04-27 21:18:46 -0500

    firtsCommitPorfolio

---
+++ scripts.js
@@ -0,0 +1,5 @@
+
+// Animaciones sencillas (más adelante podemos meter framer motion o AOS si quieres)
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Portfolio Loaded.');
+});
